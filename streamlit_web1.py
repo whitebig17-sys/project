e737577cd6faff5937d5f4436c121e610c93a9b4
whitebig17-sys <whitebig17@gmail.com>
2026-09-02 13:29:00 +0800

    新增頁籤

--- streamlit_web1.py
+++ streamlit_web1.py
@@ -1,36 +1,16 @@
 import streamlit as st
 
+
 # (A) CONTENTS
+
 st.title("研究專題名稱")
 st.divider()
-st.logo("images/logo.png", size="large")
+
+# Logo
+st.logo("images/logo.png")
 
 
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown(
-        "<p style='font-size:24px;'>預防肺癌與護肺要點</p>",
-        unsafe_allow_html=True
-    )
-    st.markdown(
-        "<p style='font-size:14px;'>5大肺癌預防與護肺要點"
-        "戒菸與遠離菸害：吸菸是導致肺癌最大的危險因子</p>",
-        unsafe_allow_html=True
-    )
-
-with col2:
-    st.markdown(
-        "<p style='font-size:24px;'>治療肺癌與護肺要點</p>",
-        unsafe_allow_html=True
-    )
-
-    st.markdown(
-        "<p style='font-size:14px;'>"
-        "減少廚房油煙：高溫煎、炒容易產生有害油煙。</p>",
-        unsafe_allow_html=True
-    )
-st.divider()
+# 內容區
 c1 = st.container()
 
 with c1:
@@ -45,31 +25,55 @@ with c1:
         "防範環境與職業暴露：避免長期接觸重金屬、石棉、"
         "柴油廢氣或有害化學物質，工作時應確實穿戴防護裝備。"
     )
-st.image("images/img01.jpg", caption="肺癌預防與護肺要點")
 
-tab1, tab2, tab3 = st.tabs(["預防肺癌與護肺要點", "治療肺癌與護肺要點", "關於肺癌"])
+
+# 圖片
+st.image(
+    "images/img01.jpg",
+    caption="肺癌預防與護肺要點"
+)
+
+
+# 頁籤
+tab1, tab2 = st.tabs([
+    "預防肺癌與護肺要點",
+    "治療肺癌與護肺要點"
+])
+
+
+# Tab 1
 with tab1:
     st.header("預防肺癌與護肺要點")
-    st.image("images/img01.jpg", caption="肺癌預防與護肺要點")
 
+    st.image(
+        "images/img01.jpg",
+        caption="肺癌預防與護肺要點"
+    )
+
+
+# Tab 2
 with tab2:
     st.header("治療肺癌與護肺要點")
-    st.image("images/img01.png", caption="肺癌治療與護肺要點")
 
-with tab3:
-    st.video("https://www.youtube.com/watch?v=Bz-UCPP8fMo&t=333s", format="video/mp4", start_time=0)
+    st.image(
+        "images/img02.jpg",
+        caption="肺癌治療與護肺要點"
+    )
 
 
-# (B) LEFT
+# (B) LEFT / SIDEBAR
+
 with st.sidebar:
     with st.container():
         st.header("選單標題1")
         st.write("選單內容1")
+
         st.button("按鈕A1")
         st.button("按鈕K1")
 
 
 # (C) FOOTER
+
 st.divider()
 
 st.header("關於我")
